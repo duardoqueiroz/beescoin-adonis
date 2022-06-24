6b14601dbb7e57abf2870295c654fc61e561c7c3
duardoqueiroz <eduardoldq1@gmail.com>
2022-06-23 22:18:31 -0300

    initial commit

---
+++ config/app.ts
@@ -0,0 +1,55 @@
+import proxyAddr from 'proxy-addr'
+import Env from '@ioc:Adonis/Core/Env'
+import Application from '@ioc:Adonis/Core/Application'
+import { ServerConfig } from '@ioc:Adonis/Core/Server'
+import { LoggerConfig } from '@ioc:Adonis/Core/Logger'
+import { ProfilerConfig } from '@ioc:Adonis/Core/Profiler'
+import { ValidatorConfig } from '@ioc:Adonis/Core/Validator'
+import { AssetsManagerConfig } from '@ioc:Adonis/Core/AssetsManager'
+
+export const appKey: string = Env.get('APP_KEY')
+
+export const http: ServerConfig = {
+  allowMethodSpoofing: false,
+  subdomainOffset: 2,
+  generateRequestId: false,
+  trustProxy: proxyAddr.compile('loopback'),
+  etag: false,
+  jsonpCallbackName: 'callback',
+  cookie: {
+    domain: '',
+    path: '/',
+    maxAge: '2h',
+    httpOnly: true,
+    secure: false,
+    sameSite: false,
+  },
+}
+
+export const logger: LoggerConfig = {
+  name: Env.get('APP_NAME'),
+  enabled: true,
+  level: Env.get('LOG_LEVEL', 'info'),
+  prettyPrint: Env.get('NODE_ENV') === 'development',
+}
+
+export const profiler: ProfilerConfig = {
+  enabled: true,
+  blacklist: [],
+  whitelist: [],
+}
+
+export const validator: ValidatorConfig = {}
+
+export const assets: AssetsManagerConfig = {
+  driver: Env.get('ASSETS_DRIVER'),
+  publicPath: Application.publicPath('assets'),
+  script: {
+    attributes: {
+      defer: true,
+    },
+  },
+  style: {
+    attributes: {},
+  },
+}
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const { join } = require('path')
+const Encore = require('@symfony/webpack-encore')
+
+if (!Encore.isRuntimeEnvironmentConfigured()) {
+  Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev')
+}
+
+Encore.setOutputPath('./public/assets')
+Encore.setPublicPath('/assets')
+Encore.addEntry('app', './resources/js/app.js')
+Encore.disableSingleRuntimeChunk()
+Encore.cleanupOutputBeforeBuild()
+Encore.enableSourceMaps(!Encore.isProduction())
+Encore.enableVersioning(Encore.isProduction())
+Encore.configureDevServerOptions((options) => {
+  if (!options.static) {
+    options.static = []
+  } else if (!Array.isArray(options.static)) {
+    options.static = [options.static]
+  }
+  options.liveReload = true
+  options.static.push({
+    directory: join(__dirname, './resources/views'),
+    watch: true,
+  })
+})
+
+const config = Encore.getWebpackConfig()
+config.infrastructureLogging = {
+  level: 'warn',
+}
+config.stats = 'errors-warnings'
+
+module.exports = config
